7bed91e88093fe1ba576a2a53b2240a9af35443a
enes baba <babaoglu_enes@outlook.com>
2019-11-05 12:36:21 +0300

    test view

---
+++ MVCContVersion/Controllers/CategoryController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCContVersion.Controllers
+{
+    public class CategoryController : Controller
+    {
+        
+        public CategoryController()
+        {
+
+        }
+               
+        // GET: Category
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
