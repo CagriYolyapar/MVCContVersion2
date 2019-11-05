b5371d0ff8e727e55d8eded8cc1ac109352ddc7f
mr.tarik.ozturk <mr.tarik.ozturk@hotmail.com>
2019-11-05 12:18:46 +0300

    Veritabanı OLuşturuldu

---
+++ MVCContVersion/Models/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCContVersion.Models.Entities
+{
+    public class Product:BaseEntity
+    {
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int CategoryID { get; set; }
+
+
+        //Relational Properties
+
+        public Category Category { get; set; }
+    }
+}
